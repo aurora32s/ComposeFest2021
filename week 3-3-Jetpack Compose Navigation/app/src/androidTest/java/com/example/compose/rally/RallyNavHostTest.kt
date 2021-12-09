9faf5c6ff9e1ab152ef2e15b399a04ba2496847f
aurora32s <aurora32s@naver.com>
2021-12-09 21:12:26 +0900

    [Finish]-Jetpack Compose theming

--- week 3-3-Jetpack Compose Navigation/app/src/androidTest/java/com/example/compose/rally/RallyNavHostTest.kt
+++ week 3-3-Jetpack Compose Navigation/app/src/androidTest/java/com/example/compose/rally/RallyNavHostTest.kt
@@ -3,9 +3,17 @@ package com.example.compose.rally
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.performScrollTo
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
+import org.junit.Assert.assertEquals
 import org.junit.Assert.fail
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 
@@ -15,16 +23,56 @@ class RallyNavHostTest {
     val composeTestRule = createComposeRule()
     lateinit var navController : NavHostController
 
-    @Test
-    fun rallyNavHost () {
+    @Before
+    fun setupRallyNavHost () {
         composeTestRule.setContent {
             navController = rememberNavController()
             RallyNavHost(navController = navController)
         }
+    }
+
+    @Test
+    fun rallyNavHost () {
         // it serves as a reminder to finish implementing the test
 //        fail()
         composeTestRule
             .onNodeWithContentDescription("Overview Screen")
+            .assertIsDisplayed()
+    }
+
+    @Test // Test via UI and test rule
+    fun rallyNavHost_navigateToAllAccounts_viaUI () {
+        composeTestRule
+            .onNodeWithContentDescription("All Accounts")
+            .performClick()
+        composeTestRule
+            .onNodeWithContentDescription("Accounts Screen")
+            .assertIsDisplayed() // 화면에 보여지는가?
+    }
+
+    @Test // Test via UI and navController
+    fun rallyNavHost_navigateToBills_viaUI () {
+        // When click on "All Bills"
+        composeTestRule.onNodeWithContentDescription("All Bills").apply {
+            performScrollTo()
+            performClick()
+        }
+        // Then the route is "Bills"
+        val route = navController.currentBackStackEntry?.destination?.route
+        assertEquals(route, "Bills")
+    }
+
+    // call navController.navigate directly
+    // -> Coroutines
+    @Test
+    fun rallyNavHost_navigateToAllAccounts_callingNavigate() {
+        runBlocking {
+            withContext(Dispatchers.Main) {
+                navController.navigate(RallyScreen.Accounts.name)
+            }
+        }
+        composeTestRule
+            .onNodeWithContentDescription("Accounts Screen")
             .assertIsDisplayed()
     }
 }
